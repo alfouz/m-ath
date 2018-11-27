c23767637b8a2439ba45e2768b8f0f7df31ceb5c
alfouz <adrian.lfouz@gmail.com>
2018-11-27 21:31:04 +0100

    add internacionalización en enumerados y contexto de app estático

--- app/src/main/java/com/alfouz/tfm/tfm/Util/CourseType.java
+++ app/src/main/java/com/alfouz/tfm/tfm/Util/CourseType.java
@@ -1,5 +1,9 @@
 package com.alfouz.tfm.tfm.Util;
 
+
+import com.alfouz.tfm.tfm.MyApplication;
+import com.alfouz.tfm.tfm.R;
+
 public enum CourseType {
     Maths(1), Science(2), Engineering(3), Technology(4), Others(5);
 
@@ -20,5 +24,22 @@ public enum CourseType {
             }
         }
         return CourseType.Others;
+    }
+
+    @Override
+    public String toString() {
+        switch (id){
+            case 1:
+                return MyApplication.getAppContext().getString(R.string.course_type_maths);
+            case 2:
+                return MyApplication.getAppContext().getString(R.string.course_type_science);
+            case 3:
+                return MyApplication.getAppContext().getString(R.string.course_type_engineering);
+            case 4:
+                return MyApplication.getAppContext().getString(R.string.course_type_technology);
+            case 5:
+                return MyApplication.getAppContext().getString(R.string.course_type_others);
+        }
+        return super.toString();
     }
 }
